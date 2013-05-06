9b542f3ebd0c040f54b4f14d06b1eb0ec8bcbb8f
kwame <jnaadjie@afrails.com>
2013-05-06 15:28:10 +0000

    sign-in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  before_filter :authorize, only: [:edit, :update]
   def index
   end
 
@@ -13,12 +13,34 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(params[:user])
-   	if @user.save
-    sign_in @user
-	   flash[:success]= "Welcome to Rails Tutorial"
-       redirect_to @user
-	else
-	  render 'new'
-	end
+    	if @user.save
+       sign_in @user
+	      flash[:success]= "Welcome to Rails Tutorial"
+        redirect_to @user
+	     else
+	         render 'new'
+       end
   end
+  
+   def edit
+    @user = User.find(params[:id])
+   end
+   
+   def update
+       @user = User.find(params[:id])
+       if @user.update_attributes(params[:user])
+           flash[:success] = "Profile successfully updated"
+           sign_in @user
+           redirect_to @user
+       else
+         render 'edit'
+       end
+   end
+   
+   private
+   
+   def authorize
+      redirect_to signin_url, notice: "Please sign in" unless signed_in?
+      
+   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,6 +11,22 @@ describe "AthenticationPages" do
                   
          end
          
+         describe "authorization" do
+                  describe "for non-signed in users" do
+                           let(:user) { FactoryGirl.create(:user) }
+                           
+                           describe "in the Users controller" do
+                                    describe "visiting the edit page" do
+                                             before { visit edit_user_path(user) }
+                                             it { should have_selector('title', text: "Sign in") }
+                                    end
+                                    describe "submitting to the update action" do
+                                             before { put user_path(user) }
+                                             specify { response.should redirect_to(signin_path) }
+                                    end
+                           end
+                  end
+         end
          describe "sign in" do
                   before { visit signin_path }
                   
@@ -36,6 +52,7 @@ describe "AthenticationPages" do
 
                    it { should have_selector('title', text: user.name) }
                    it { should have_link('Profile', href: user_path(user)) }
+                   it { should have_link('Settings', href: edit_user_path(user)) }
                    it { should have_link('Sign out', href: signout_path) }
                    it { should_not have_link('Sign in', href: signin_path) }
                         
